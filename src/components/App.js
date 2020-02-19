0ef201531740bf5ea31a232984a3edc5ea5e7ad2
James <james.duong93@gmail.com>
2020-02-19 14:19:50 +1100

    data test attributes

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,8 @@ import React from "react";
 
 const App = ({ user }) => (
   <div>
-    <h1>{user.firstName}</h1>
+    <h1 data-test="firstName">{user.firstName}</h1>
+    <h2 data-test="lastName">{user.lastName}</h2>
   </div>
 );
 
